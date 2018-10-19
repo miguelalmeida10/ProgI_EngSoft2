519080f9fad85d47313f99745d648bb3678bb622
miguelalmeida10 <33085795+miguelalmeida10@users.noreply.github.com>
2018-10-19 12:41:41 +0100

    update models

--- HospitalScheduling/HospitalScheduling/Models/Doctor.cs
+++ HospitalScheduling/HospitalScheduling/Models/Doctor.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace HospitalScheduling.Models
 {
-    public class Doctor
+    public class Doctor : Employee
     {
-        public Doctor()
-        {
-        }
+        public int EmployeeID { get; set; }
+
+        //name
+        [Required(ErrorMessage = "Please enter the name of the Nurse")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        //email
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //phone
+        [Required(ErrorMessage = "Please enter the phone number")]
+        [RegularExpression(@"(9[1236]|2\d)\d{7}")]
+        public string Phone { get; set; }
+
+        //birthday
+        public DateTime Birthday { get; set; }
+
+        //adress
+        public string Adress { get; set; }
     }
 }
