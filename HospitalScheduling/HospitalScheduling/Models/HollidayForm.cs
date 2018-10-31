fa0207875ded588a26a1c348191c030ecfc4c4c6
beratdevecioglu <36261703+beratdevecioglu@users.noreply.github.com>
2018-10-31 22:56:34 +0000

    Hollıday Form

---
+++ HospitalScheduling/HospitalScheduling/Models/HollidayForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalScheduling.Models
+{
+    public class HollidayForm
+    {
+        [Required(ErrorMessage = "Please, enter the starting date of your vacation")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Datestart { get; set; }
+
+        [Required(ErrorMessage = "Please, enter the ending date of your vacation")]
+        public string Dateend { get; set; }
+    }
+}
