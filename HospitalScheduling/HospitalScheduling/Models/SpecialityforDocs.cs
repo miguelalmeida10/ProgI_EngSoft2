0e5cc7a38902bb79fe870d0eb365222022756af3
beratdevecioglu <36261703+beratdevecioglu@users.noreply.github.com>
2018-11-02 15:42:48 +0000

    update

---
+++ HospitalScheduling/HospitalScheduling/Models/SpecialityforDocs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalScheduling.Models
+
+{
+    public class SpecialityforDocs
+    {
+        
+        public int SpecialityforDocsID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
